33c68556e8cb96431e1c87af8b07360df6342def
RatushnyiArtem <92036528+RatushnyiArtem@users.noreply.github.com>
2025-04-20 16:27:08 +0200

    12

---
+++ Calculator/ButtonForCalc.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buttonsForCalc
+{
+    public class ButtonForCalc:Button
+    {
+        private int borderSize = 0;
+        private int borderRadius = 50;
+        private Color borderColor = Color.Transparent;
+
+        public int BorderSize
+        {
+            get => borderSize;
+            set { borderSize = (value<=Height)? value: Height; Invalidate(); }
+        }
+        public int BorderRadius
+        {
+            get => borderRadius;
+            set { borderRadius = value; Invalidate(); }
+        }
+        public Color TextColor
+        {
+            get => ForeColor;
+            set { ForeColor = value; Invalidate(); }
+        }
+
+        public ButtonForCalc()
+        {
+            Size = new Size(200, 100);
+            FlatAppearance.BorderSize = 0;
+            FlatStyle = FlatStyle.Flat;
+            BackColor = Color.DodgerBlue;
+            ForeColor = Color.White;
+
+            Resize += new EventHandler(Button_Resize);
+        }
+
+        private void Button_Resize(object sender, EventArgs e)
+        {
+            if(borderRadius > Height) borderRadius = Height;
+        }
+
+        private GraphicsPath GetFigurePath(RectangleF rectangle, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(rectangle.X, rectangle.Y, radius, radius, 180, 90);
+            path.AddArc(rectangle.Width - radius, rectangle.Y, radius, radius, 270, 90);
+            path.AddArc(rectangle.Width - radius, rectangle.Height - radius, radius, radius, 0, 90);
+            path.AddArc(rectangle.X, rectangle.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+
+
+            return path;
+        }
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            RectangleF rectangleSurface = new RectangleF(0, 0, Width, Height);
+            RectangleF rectangleBorder = new RectangleF(1, 1, Width - 0.5F, Height - 1);
+
+            if (borderRadius > 1) 
+            {
+                using (GraphicsPath graphicPathSurface = GetFigurePath(rectangleSurface, borderRadius))
+                using (GraphicsPath graphicPathBorder = GetFigurePath(rectangleBorder, borderRadius - 1F))
+                using(Pen penSurface = new Pen(Parent.BackColor,2))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    penBorder.Alignment = PenAlignment.Center;
+                    Region = new Region(graphicPathSurface);
+                    pevent.Graphics.DrawPath(penBorder,graphicPathSurface);
+
+                    if(borderSize >= 1)
+                    {
+                        pevent.Graphics.DrawPath(penBorder, graphicPathBorder);
+                    }
+                }
+            }
+            else
+            {
+                Region = new Region(rectangleSurface);
+                    if(borderSize >= 1)
+                        using(Pen penBorder = new Pen(borderColor, borderSize))
+                        {
+                            penBorder.Alignment = PenAlignment.Inset;
+                            pevent.Graphics.DrawRectangle(penBorder,0,0,Width-1,Height - 1);
+                        }
+            }
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            Parent.BackColorChanged += new EventHandler(Container_BackColorChanged);
+        }
+        private void Container_BackColorChanged(object sender, EventArgs e)
+        {
+            if (DesignMode) Invalidate(); 
+        }
+    }
+}
---
+++ Calculator/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using buttonsForCalc;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonForCalc1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonForCalc3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonForCalc9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Double result = 0;
+        string operation = string.Empty;
+        string fstNum, sndNum;
+        bool enterValue = false;
+
+        private void BtnMathOperation_Click(object sender, EventArgs e)
+        {
+            if (result != 0) BtnEquals.PerformClick();
+            else result = Double.Parse(TextDisplay1.Text);
+
+            ButtonForCalc button = (ButtonForCalc)sender;
+            operation = button.Text;
+            enterValue = true;
+            if(TextDisplay1.Text != "0") {
+                TextDisplay2.Text = fstNum = $"{result} {operation}";
+                TextDisplay1.Text = string.Empty;
+            }
+        }
+
+        private void BtnEqual_Click(object sender, EventArgs e)
+        {
+            sndNum = TextDisplay1.Text;
+            TextDisplay2.Text = $"{TextDisplay2.Text} {TextDisplay1.Text}=";
+            if (TextDisplay1.Text != string.Empty)
+            {
+                if (TextDisplay1.Text == "0") TextDisplay2.Text = string.Empty;
+                switch (operation)
+                {
+                    case "+":
+                        TextDisplay1.Text =(result+Double.Parse(TextDisplay1.Text)).ToString();
+                        RtBoxDisplayHistory.AppendText($"{fstNum} {sndNum} = {TextDisplay1.Text}\n");
+                        break;
+                    case "-":
+                        TextDisplay1.Text = (result - Double.Parse(TextDisplay1.Text)).ToString();
+                        RtBoxDisplayHistory.AppendText($"{fstNum} {sndNum} = {TextDisplay1.Text}\n");
+                        break;
+                    case "×":
+                        TextDisplay1.Text = (result * Double.Parse(TextDisplay1.Text)).ToString();
+                        RtBoxDisplayHistory.AppendText($"{fstNum} {sndNum} = {TextDisplay1.Text}\n");
+                        break;
+                    case "÷":
+                        TextDisplay1.Text = (result / Double.Parse(TextDisplay1.Text)).ToString();
+                        RtBoxDisplayHistory.AppendText($"{fstNum} {sndNum} = {TextDisplay1.Text}\n");
+                        break;
+                    default: TextDisplay2.Text = $"{TextDisplay1.Text} = ";
+                        break;
+                }
+
+
+                result = Double.Parse(TextDisplay1.Text);
+                operation = string.Empty;
+            }
+               
+        }
+
+        private void ButtonHistory_Click(object sender, EventArgs e)
+        {
+            PanelHistory.Height = (PanelHistory.Height == 5) ? PanelHistory.Height = 398 : 5;
+        }
+
+        private void ButtonClearHistory_Click(object sender, EventArgs e)
+        {
+            RtBoxDisplayHistory.Clear();
+            if (RtBoxDisplayHistory.Text == string.Empty)
+                RtBoxDisplayHistory.Text = "There is no history yet.";
+
+        }
+
+        private void BtnBackSpace_Click(object sender, EventArgs e)
+        {
+            if (TextDisplay1.Text.Length > 0)
+                TextDisplay1.Text = TextDisplay1.Text.Remove(TextDisplay1.Text.Length - 1, 1);
+            if (TextDisplay1.Text == string.Empty) TextDisplay1.Text = "0";
+        }
+
+        private void BtnC_Click(object sender, EventArgs e)
+        {
+            TextDisplay1.Text = "0";
+            TextDisplay2.Text = string.Empty;
+            result = 0;
+        }
+
+        private void BtnCE_Click(object sender, EventArgs e)
+        {
+            TextDisplay1.Text = "0";
+        }
+
+        private void ButtonOperations_Click(object sender, EventArgs e)
+        {
+            ButtonForCalc button = (ButtonForCalc)sender;
+            operation = button.Text;
+            switch (operation) {
+                case "√x":
+                    TextDisplay2.Text = $"√({TextDisplay1.Text})";
+                    TextDisplay1.Text = Convert.ToString(Math.Sqrt(Double.Parse(TextDisplay1.Text)));
+                    break;
+                case "х²":
+                    TextDisplay2.Text = $"({TextDisplay1.Text})²";
+                    TextDisplay1.Text = Convert.ToString(Convert.ToDouble(TextDisplay1.Text)*(Convert.ToDouble(TextDisplay1.Text)));
+                    break;
+                case "¹/x":
+                    TextDisplay2.Text = $"¹/({TextDisplay1.Text})";
+                    TextDisplay1.Text = Convert.ToString(1.0 / Convert.ToDouble(TextDisplay1.Text));
+                    break;
+                case "%":
+                    TextDisplay2.Text = $"({TextDisplay1.Text})%";
+                    TextDisplay1.Text = Convert.ToString(Convert.ToDouble(TextDisplay1.Text) / Convert.ToDouble(100));
+                    break;
+                case "±":
+                    TextDisplay1.Text = Convert.ToString(-1 * Convert.ToDouble(TextDisplay1.Text));
+                    break;
+            }
+            RtBoxDisplayHistory.AppendText($"{TextDisplay2.Text} = {TextDisplay1.Text} \n");
+        }
+
+        private void ButtonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ButtonHide_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void ButtonFullScreen_Click(object sender, EventArgs e)
+        {
+            //if(WindowState == FormWindowState.Normal) WindowState = FormWindowState.Maximized;
+            //else WindowState = FormWindowState.Normal;
+        }
+
+        private void BtnNum_Click(object sender, EventArgs e)
+        {
+            if (TextDisplay1.Text == "0" || enterValue) TextDisplay1.Text = string.Empty;
+            enterValue = false;
+            ButtonForCalc button = (ButtonForCalc)sender;
+            if (button.Text == ".")
+            {
+                if (!TextDisplay1.Text.Contains("."))
+                    TextDisplay1.Text = TextDisplay1.Text + button.Text;
+            }
+            else TextDisplay1.Text = TextDisplay1.Text + button.Text;
+        }
+    }
+}
